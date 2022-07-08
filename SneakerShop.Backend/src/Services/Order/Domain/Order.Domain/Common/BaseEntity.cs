7921d446914ecdd995396205d7f1ceece7be0edb
TkachukSasha <sashatkachuk09@ukr.net>
2022-07-09 00:45:15 +0300

    #0002 feat: create entities for order service

---
+++ SneakerShop.Backend/src/Services/Order/Domain/Order.Domain/Common/BaseEntity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Order.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public int OrderId { get; set; }
+
+        public List<BaseEvent> _domainEvents = new List<BaseEvent>();
+
+        [NotMapped]
+        public IReadOnlyCollection<BaseEvent> DomainEvents => _domainEvents;
+
+        public void AddDomainEvent(BaseEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        public void RemoveDomainEvent(BaseEvent domainEvent)
+        {
+            _domainEvents.Remove(domainEvent);
+        }
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
+    }
+}
