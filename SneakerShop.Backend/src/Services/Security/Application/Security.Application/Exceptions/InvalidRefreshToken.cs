3752e43747378e29eaa30db3cab1fd758b4e2f1d
TkachukSasha <sashatkachuk09@ukr.net>
2022-07-11 09:58:05 +0300

    #0006 feat: add refresh token command logic, and logout endpoint

---
+++ SneakerShop.Backend/src/Services/Security/Application/Security.Application/Exceptions/InvalidRefreshToken.cs
@@ -0,0 +1,12 @@
+﻿namespace Security.Application.Exceptions
+{
+    public class InvalidRefreshToken : CustomException
+    {
+        public string Token { get; }
+
+        public InvalidRefreshToken(string token) : base($"Refresh token: {token} is invalid!")
+        {
+            Token = token;
+        }
+    }
+}
