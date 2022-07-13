e78955d2d596f5f64a835b0f9122e8be1b704849
Illia-creator <sie29092002@gmail.com>
2022-07-13 17:35:31 +0300

    #0008 feat: basket microservice without swagger

---
+++ SneakerShop.Backend/src/Services/Basket/Api/Basket.Api/Controllers/BasketController.cs
@@ -0,0 +1,46 @@
+﻿using Basket.Api.Repositories;
+using Basket.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Security.Domain.ValueObjects;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Basket.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+        
+        
+        [HttpGet("{login}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK )]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string login)
+        {
+            var basket = await _repository.GetBasketAsync(login);
+            return Ok(basket ?? new ShoppingCart(login));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            return Ok(await _repository.UpdateBasketAsync(basket));
+        }
+
+        [HttpDelete("{login}", Name = "{DeleteBasket}")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string login)
+        { 
+            await _repository.DeleteBasketAsync(login);
+            return Ok();        
+        }
+    }
+}
