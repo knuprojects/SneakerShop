5ae8a382ae7cd0314e3b258b2634a8855c0e1dc2
Illia-creator <sie29092002@gmail.com>
2022-07-16 18:36:38 +0300

    #0008 fix: removed unnecessary elements

---
+++ SneakerShop.Backend/src/Services/Basket/Domain/Basket.Domain/Const/Routes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basket.Domain.Const
+{
+    public static class Routes
+    {
+        public const string GetBasketByLogin = "/basket/{login}";
+        public const string PostBasket = "/basket";
+        public const string DeleteBasket = "/basket/{login}";
+    }
+}
