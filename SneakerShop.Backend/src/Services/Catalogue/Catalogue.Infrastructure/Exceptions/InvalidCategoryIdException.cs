a96526104d4ba2491dfa97af10cbd69499a48943
Illia-creator <sie29092002@gmail.com>
2022-07-21 22:08:57 +0300

    #0020 feat: refactor bad refactored exceptions

---
+++ SneakerShop.Backend/src/Services/Catalogue/Catalogue.Infrastructure/Exceptions/InvalidCategoryIdException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalogue.Infrastructure.Exceptions
+{
+    public sealed class InvalidCategoryIdException : CustomException
+    {
+        public int Value { get; }
+        public InvalidCategoryIdException(int value) : base($"Category id: {value} is invalid!")
+        {
+            Value = value;
+        }
+    }
+}
---
+++ SneakerShop.Backend/src/Services/Catalogue/Catalogue.Infrastructure/Exceptions/InvalidCompanyIdException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalogue.Infrastructure.Exceptions
+{
+    public sealed class InvalidCompanyIdException : CustomException
+    {
+        public int Value { get; }
+        public InvalidCompanyIdException(int value) : base($"Company id: {value} is invalid!")
+        {
+            Value = value;
+        }
+    }
+}
